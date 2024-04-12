774d764e558aaac11db94caa10856c423e827d9e
unknown <rajkousik20@gmail.com>
2024-04-12 16:47:23 +0530

    Modified Functions

--- Day4/FirstFullSolutions/DoctorApplication/Program.cs
+++ Day4/FirstFullSolutions/DoctorApplication/Program.cs
@@ -4,7 +4,10 @@ namespace DoctorApplication
 {
     internal class Program
     {
-
+        /// <summary>
+        /// Creates a new doctor by taking inputs from the user
+        /// </summary>
+        /// <returns>a object of type 'Doctor'</returns>
         static Doctor CreateNewDoctor()
         {
             Console.WriteLine("Enter the details of the new doctor:");
@@ -42,14 +45,61 @@ namespace DoctorApplication
             return newDoctor;
         }
 
+        /// <summary>
+        /// Print the doctor details
+        /// </summary>
+        /// <param name="doctors">Array of 'Doctor' objects passed as reference</param>
+        private static void PrintDoctorDetails(ref Doctor[] doctors)
+        {
+            Console.WriteLine("All Doctors: ");
+            foreach (var doctor in doctors)
+            {
+                doctor.PrintDoctorDetails();
+            }
+        }
+
+        /// <summary>
+        /// Search Functionality to filter doctors based on speciality
+        /// </summary>
+        /// <param name="doctors">Array of 'Doctor' objects passed as reference</param>
+        private static void SearchBySpeciality(ref Doctor[] doctors)
+        {
+
+            Console.WriteLine("Enter a specialty to search for doctors:");
+            string? searchSpecialty = Console.ReadLine();
+
+            Console.WriteLine($"Doctors with specialty {searchSpecialty}:");
+            foreach (var doctor in doctors)
+            {
+                if (doctor.Speciality.Equals(searchSpecialty, StringComparison.OrdinalIgnoreCase))
+                {
+                    doctor.PrintDoctorDetails();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds a new doctor
+        /// </summary>
+        /// <param name="doctors">Array of 'Doctor' objects passed as reference</param>
+        private static void AddNewDoctor(ref Doctor[] doctors)
+        {
+
+            Doctor newDoctor = CreateNewDoctor();
+            Array.Resize(ref doctors, doctors.Length + 1);
+            doctors[doctors.Length - 1] = newDoctor;
+
+            Console.WriteLine("New doctor added successfully!");
+        }
+
         static void Main(string[] args)
         {
-            int CurrentDoctorCount = 4;
+            const int CurrentDoctorCount = 4;
             Doctor[] doctors = new Doctor[CurrentDoctorCount];
             doctors[0] = new Doctor(1, "Dr. Smith", 45, 20, "MD", "Cardiology");
             doctors[1] = new Doctor(2, "Dr. Jones", 50, 25, "PhD", "Neurology");
             doctors[2] = new Doctor(3, "Dr. Patel", 40, 15, "MBBS", "Pediatrics");
-            doctors[3] = new Doctor(3, "Dr. Raj Kousik", 45, 10, "MD", "Cardiology");
+            doctors[3] = new Doctor(4, "Dr. Raj Kousik", 45, 10, "MD", "Cardiology");
 
             bool exit = false;
             while (!exit)
@@ -66,31 +116,13 @@ namespace DoctorApplication
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("All Doctors:");
-                        foreach (var doctor in doctors)
-                        {
-                            doctor.PrintDoctorDetails();
-                        }
+                        PrintDoctorDetails(ref doctors);
                         break;
                     case 2:
-                        Console.WriteLine("Enter a specialty to search for doctors:");
-                        string? searchSpecialty = Console.ReadLine();
-
-                        Console.WriteLine($"Doctors with specialty {searchSpecialty}:");
-                        foreach (var doctor in doctors)
-                        {
-                            if (doctor.Speciality.Equals(searchSpecialty, StringComparison.OrdinalIgnoreCase))
-                            {
-                                doctor.PrintDoctorDetails();
-                            }
-                        }
+                        SearchBySpeciality(ref doctors);
                         break;
                     case 3:
-                        Doctor newDoctor = CreateNewDoctor();
-                        Array.Resize(ref doctors, doctors.Length + 1);
-                        doctors[doctors.Length - 1] = newDoctor;
-
-                        Console.WriteLine("New doctor added successfully!");
+                        AddNewDoctor(ref doctors);
                         break;
                     case 4:
                         exit = true;
--- Day4/FirstFullSolutions/ValidityCheck/Program.cs
+++ Day4/FirstFullSolutions/ValidityCheck/Program.cs
@@ -8,6 +8,7 @@ namespace ValidityCheck
         static void Main(string[] args)
         {
 
+            Console.Write("Enter a Number: ");
             string StrNum = Console.ReadLine()??"";
             string RevNum = ReverseString(StrNum);
 
@@ -15,31 +16,63 @@ namespace ValidityCheck
 
             for(int i = 0; i < RevNum.Length; i++)
             {
-                int Digit = RevNum[i] - 48;
+                int Digit = RevNum[i] - '0';
                 if (i%2 == 1)
                 {
                     Digit *= 2;
-                    while(Digit > 10)
+                    while(Digit > 9)
                     {
                         Digit = FindSum(Digit);
+                        //Digit -= 9; Even this works exactly as same as FindSum Function
                     }
                 }
                 TotalSum += Digit;
             }
 
-            if(TotalSum % 10 == 0)
+            if(ValidityCheck(TotalSum))
                 Console.WriteLine("Yes...The Given Number is Valid");
             else
                 Console.WriteLine("Oops...Not an valid numbere");
         }
 
+        private static bool ValidityCheck(int Number)
+        {
+            return (Number % 10 == 0);
+        }
 
+        /* 
+          maximum possible value if we multiply by 2 is 18
+            18 - 9 = 9
+            17 - 9 = 8
+            16 - 9 = 7
+            15 - 9 = 6
+            14 - 9 = 5
+            13 - 9 = 4
+            12 - 9 = 3
+            11 - 9 = 3
+            10 - 9 = 1
+            
+            So subtracting 9 from the number will give the sum of digits
+         */
+
+
+        /// <summary>
+        /// Function reverses a given string
+        /// </summary>
+        /// <param name="Str">String to be reversed</param>
+        /// <returns>Reversed string</returns>
         private static string ReverseString(string Str)
         {
             char[] CharArray = Str.ToCharArray();
             Array.Reverse(CharArray);
             return new string(CharArray);
         }
+
+        /// <summary>
+        /// Find sum of all the digits in a given number
+        /// </summary>
+        /// <param name="Num">A Number whose sum to be find</param>
+        /// <returns>Returns the sum of all the digits in a number</returns>
 
         private static int FindSum(int Num)
         {
