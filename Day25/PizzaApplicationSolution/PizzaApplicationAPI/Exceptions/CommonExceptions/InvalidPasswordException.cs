e4e71acd20660491da508e67ee5a7aa7c54021b8
Kousik <rajkousik20@gmail.com>
2024-05-30 19:18:16 +0530

    minor fix

--- Day25/PizzaApplicationSolution/PizzaApplicationAPI/Exceptions/CommonExceptions/InvalidPasswordException.cs
+++ Day25/PizzaApplicationSolution/PizzaApplicationAPI/Exceptions/CommonExceptions/InvalidPasswordException.cs
@@ -8,7 +8,7 @@ namespace PizzaApplicationAPI.Exceptions.CommonExceptions
         private string message;
         public InvalidPasswordException()
         {
-            message = "Your password should contain at least one lowercase letter, one uppercase letter, one number, and one special character. It should be a minimum of 8 characters in length.";
+            message = "Your password should contain at least one lowercase letter, one uppercase letter, one number,  one special character and also a minimum of 8 characters in length.";
         }
 
         public InvalidPasswordException(string message)
